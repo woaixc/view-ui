906ea32e9a6a6294c840a3212963bf5fe0c17451
woaixc <1512889168@qq.com>
2020-08-05 15:59:48 +0800

    1

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,40 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
-
+import Vuex, { Store } from 'vuex'
+  
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    counter: 0,
+    student: [
+      {name:"xiecong",age:18,height:1.88},
+      {name:"xiecong1",age:188,height:1.88},
+      {name:"xiecong2",age:1888,height:1.88},
+      {name:"xiecong3",age:18888,height:1.88},
+      {name:"xiecong4",age:188888,height:1.88}
+    ]
   },
   mutations: {
+    increment(state){
+      state.counter++
+    },
+    decrement(state){
+      state.counter--
+    },
+    incrementNum(state,payload){
+      state.counter+=payload.count,
+      console.log(payload.payload)
+    }
   },
   actions: {
   },
   modules: {
+  },
+  getters: { 
+    students(state){
+      return state.student.filter(s=> {
+        return s.age>=18
+      })
+    }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,14 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <h2>{{this.$store.state.counter}}</h2>
+    <button @click='add'>+</button>
+    <button @click='sub'>-</button>
+    <button @click="addition(5)">+5</button>
+    <button @click="addition(10)">+10</button>
+
+    <h2>{{this.$store.getters.students}}</h2>
   </div>
 </template>
 
@@ -13,6 +21,22 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  methods: {
+    add() {
+      this.$store.commit('increment')
+    },
+    sub() {
+      this.$store.commit('decrement')
+    },
+    addition(count){
+      // this.$store.commit('incrementNum',count)
+      this.$store.commit({
+        type: 'incrementNum',
+        count: 5,
+        payload: 'payload'
+      })
+    }
   }
 }
 </script>
